2ab2184128242f8ebbef8dc45151118e5a51fe04
arem <andrewvillalon123@gmail.com>
2022-10-31 00:00:55 +0800

    added warning feature

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 // variables
 const addBtn = document.querySelector("#addBtn");
 const ul = document.querySelector("#list-item-group");
+const msg = document.querySelector(".msg");
 
 addBtn.addEventListener("click", addItem);
 
 function addItem(e) {
   e.preventDefault();
   const inputValue = document.querySelector("#addInput").value;
-  const li = document.createElement("li");
-  li.className = "list-items";
-  li.appendChild(document.createTextNode(inputValue));
-  ul.appendChild(li);
-  const deleteBtn = document.createElement("button");
-  deleteBtn.className = "ri-delete-bin-fill deleteBtn";
-  li.appendChild(deleteBtn);
+  if (inputValue == "" || inputValue == null) {
+    msg.classList.add("warning");
+    msg.innerHTML = "Please type first!";
+    //setTimeout(() => msg.remove(), 3000);
+  } else if (inputValue != "" || inputValue != null) {
+    msg.remove();
+    const li = document.createElement("li");
+    li.className = "list-items";
+    li.appendChild(document.createTextNode(inputValue));
+    ul.appendChild(li);
+    // creating delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "ri-delete-bin-fill deleteBtn";
+    li.appendChild(deleteBtn);
+  }
 }
 
 ul.addEventListener("click", removeItem);
